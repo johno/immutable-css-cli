43429af6cc0c5ad9fc15669df3e989d1b8890300
John Otander <johnotander@gmail.com>
2015-10-22 14:29:54 -0600

    Begin adding basic cli functionality

--- cli.js
+++ cli.js
@@ -1,7 +1,40 @@
+#!/usr/bin/env node
 'use strict'
 
-module.exports = function immutableCssCli (options) {
-  options = options || {}
+const fs = require('fs')
+const meow = require('meow')
+const chalk = require('chalk')
+const isCss = require('is-css')
+const isGlob = require('is-glob')
+const postcss = require('postcss')
+const isBlank = require('is-blank')
+const fileExists = require('file-exists')
+const atImport = require('postcss-import')
+const reporter = require('postcss-reporter')
+const immutableCss = require('immutable-css')
 
-  return true
+const cli = meow(`
+  Usage
+    $ immutable-css <vendor-css-file> <app-css-file>
+    $ immutable-css <glob>
+
+  Options
+    -j, --json Return json to std out
+
+  Example
+    $ immutable-css vendor.css app.css
+    $ immutable-css src/css/**/*.css
+    $ immutable-css src/css/**/*.css --json > mutations.json
+`, {
+  alias: {
+    j: 'json'
+  }
+})
+
+const vendorFileOrGlob = cli.input[0]
+const appFile = cli.input[1]
+
+if (isBlank(vendorFileOrGlob)) {
+  console.error(chalk.red('Please provide CSS files or a glob\n') + cli.help)
+  process.exit(1)
 }
